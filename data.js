227ce282ad50bea72ec9e3e759a04cdb0d159b77
Indian121 <110817782+Indian121@users.noreply.github.com>
2022-08-08 14:46:50 +0530

    Church VR

---
+++ data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-scene1",
+      "name": "Scene1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02557801929021153,
+          "pitch": 0.1490567327425083,
+          "rotation": 0,
+          "target": "1-scene2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-scene2",
+      "name": "Scene2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0029456520937714004,
+          "pitch": 0.2701239458814868,
+          "rotation": 0,
+          "target": "2-scene3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-scene3",
+      "name": "Scene3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8468564358684318,
+          "pitch": 0.25206040902939186,
+          "rotation": 0,
+          "target": "3-scene4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-scene4",
+      "name": "Scene4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
